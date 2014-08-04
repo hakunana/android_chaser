b5e26f0ae91433d8956c17b95538c31a698b15ff
Alexander Seipel <alexander.seipel@gmail.com>
2014-08-04 14:14:38 +0200

    added constructor to Track

--- AdventureRun/src/de/ur/mi/android/adventurerun/data/Track.java
+++ AdventureRun/src/de/ur/mi/android/adventurerun/data/Track.java
@@ -2,6 +2,10 @@ package de.ur.mi.android.adventurerun.data;
 
 public class Track {
 
+	private ArrayList<Checkpoint> checkpoints;
 	
+	public Track(ArrayList<Checkpoint> checkpoints) {
+		this.checkpoints = checkpoints;
+	}
 
 }
