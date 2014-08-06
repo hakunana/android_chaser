69a6da244b0fbe79c26967fe1fe4406601835c24
Alexander Seipel <alexander.seipel@gmail.com>
2014-08-06 20:13:42 +0200

    CreateView
    Create Control object

--- AdventureRun/src/de/ur/mi/android/adventurerun/view/CreateView.java
+++ AdventureRun/src/de/ur/mi/android/adventurerun/view/CreateView.java
@@ -1,16 +1,22 @@
 package de.ur.mi.android.adventurerun.view;
 
-import com.example.adventurerun.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.example.adventurerun.R;
+
+import de.ur.mi.android.adventurerun.control.CreateControl;
+
 public class CreateView extends Activity {
+	
+	private CreateControl control;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.createview);
+		
+		control = new CreateControl();
 	}
 	
 }
